ec30fcc02ecc7a432ad0f7add52ba790f185b90c
zenghuan1 <huan.zeng1@pactera.com>
2018-06-08 19:57:28 +0800

    Adjust its way
    Signed-off-by: zenghuan1 <huan.zeng1@pactera.com>

--- SDL_Qt_HMI/main.cpp
+++ SDL_Qt_HMI/main.cpp
@@ -4,25 +4,26 @@
 #include <QDialog>
 #include <QTimer>
 #include "MainWindow.h"
+
 int main(int argc, char *argv[]) {
-  QApplication a(argc, argv);
+    QApplication a(argc, argv);
 
 #if (QT_VERSION <= QT_VERSION_CHECK(5,0,0))
-  QTextCodec *codec = QTextCodec::codecForName("UTF-8");
-  QTextCodec::setCodecForLocale(codec);
-  QTextCodec::setCodecForCStrings(codec);
-  QTextCodec::setCodecForTr(codec);
+    QTextCodec *codec = QTextCodec::codecForName("UTF-8");
+    QTextCodec::setCodecForLocale(codec);
+    QTextCodec::setCodecForCStrings(codec);
+    QTextCodec::setCodecForTr(codec);
 #else
-  QTextCodec *codec = QTextCodec::codecForName("UTF-8");
-  QTextCodec::setCodecForLocale(codec);
+    QTextCodec *codec = QTextCodec::codecForName("UTF-8");
+    QTextCodec::setCodecForLocale(codec);
 #endif
 
-   qRegisterMetaType<string>("string");
-   qRegisterMetaType< map<string,string> >("map<string,string>");
+    qRegisterMetaType<string>("string");
+    qRegisterMetaType< map<string,string> >("map<string,string>");
 
-   MainWindow w;
-   w.setWindowFlags(Qt::FramelessWindowHint);
-   w.resize(800,480);
-   w.show();
-   return a.exec();
+    MainWindow w;
+    w.setWindowFlags(Qt::FramelessWindowHint);
+    w.resize(800,480);
+    w.show();
+    return a.exec();
 }
