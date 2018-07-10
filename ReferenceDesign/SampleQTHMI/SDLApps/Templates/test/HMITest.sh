dd026e162ab584b271645798b2a609b91ca0d7c1
jinhui.guan <jinhui.guan@pactera.com>
2018-07-10 17:17:21 +0800

    add coverage test

--- ReferenceDesign/SampleQTHMI/SDLApps/Templates/test/HMITest.sh
+++ ReferenceDesign/SampleQTHMI/SDLApps/Templates/test/HMITest.sh
@@ -1,6 +1,8 @@
 export LD_LIBRARY_PATH=.
 ./HMI_TEST
-lcov -c -o test.info -d CMakeFiles/HMI_TEST.dir
-genhtml test.info -o ./TestResult
+lcov -c -o test.info -d ../../
+lcov --extract test.info '*/SDLApps/Templates/*' -o result.info
+genhtml result.info -o ./TestResult
+
 
 
