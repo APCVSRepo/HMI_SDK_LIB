47416035ab983bcc2e5ed877cd1d2b315f658f62
jinhui.guan <jinhui.guan@pactera.com>
2018-07-16 11:48:53 +0800

    update HMITest.sh

--- ReferenceDesign/SampleQTHMI/SDLApps/Templates/test/HMITest.sh
+++ ReferenceDesign/SampleQTHMI/SDLApps/Templates/test/HMITest.sh
@@ -2,7 +2,5 @@ export LD_LIBRARY_PATH=.
 ./HMI_TEST
 lcov -c -o test.info -d ../../
 lcov --extract test.info '*/SDLApps/Templates/*' -o result.info
-genhtml result.info -o ./TestResult
-mv TestResult ../../
-
+genhtml result.info -o ../../TestResult
 
