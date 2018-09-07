02d518baaa462bef6b9709d7ab05e0b717c42a7f
jinhui.guan <jinhui.guan@pactera.com>
2018-09-07 16:00:38 +0800

    update function of mocked object, add gtest file for DialNumber

---
+++ ReferenceDesign/SampleQTHMI/SDLApps/Templates/DialNumber/test/DialNumberTest.cpp
@@ -0,0 +1,31 @@
+#include "DialNumberTest.h"
+
+using namespace test;
+using namespace hmi_sdk;
+using namespace rpc_test;
+
+
+CDialNumberTest::CDialNumberTest()
+{
+
+}
+
+void CDialNumberTest::SetUpTestCase()
+{
+
+}
+
+void CDialNumberTest::TearDownTestCase()
+{
+
+}
+
+void CDialNumberTest::SetUp()
+{
+
+}
+
+void CDialNumberTest::TearDown()
+{
+
+}
