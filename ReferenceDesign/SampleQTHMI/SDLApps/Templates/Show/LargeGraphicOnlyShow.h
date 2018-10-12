4f9dc0cd8652c5ac1b8c58f9932659d4d153a4c1
zenghuan1 <huan.zeng1@pactera.com>
2018-10-12 15:18:11 +0800

    add new templates LargeGraphicOnly,TextButtonsOnly,TextButtonsWithGraphic
    Signed-off-by: zenghuan1 <huan.zeng1@pactera.com>

---
+++ ReferenceDesign/SampleQTHMI/SDLApps/Templates/Show/LargeGraphicOnlyShow.h
@@ -0,0 +1,50 @@
+#ifndef LARGERAPHICONLYSHOW_H
+#define LARGERAPHICONLYSHOW_H
+
+#include <QLabel>
+#include "SDLApps/Templates/Common/AppBase.h"
+#include "SDLApps/Templates/Common/Button.h"
+#include "HMIWidgets/AlbumCoverWidget.h"
+#include "HMIWidgets/CPushButton.h"
+#include "HMIWidgets/CSlider.h"
+#include "HMIWidgets/TopNavigateWidget.h"
+
+class CLargeGraphicOnlyShow : public QWidget {
+    Q_OBJECT
+
+public:
+    explicit CLargeGraphicOnlyShow(AppListInterface *pList, QWidget *parent = 0);
+    ~CLargeGraphicOnlyShow();
+    void SetAppName(QString strName);
+    void DeleteButton(int index);
+    void removeAllButton();
+
+    // QWidget interface
+protected:
+    void showEvent(QShowEvent *e);
+public slots:
+
+    void BtnMenuClickedSlots();
+    void BtnBackClickedSlots();
+
+private:
+    AppListInterface *m_pList;
+    //background
+    QLabel *m_pBackground;
+
+    int m_btnWidth;
+    int m_btnHeight;
+    int m_btnTop;
+    int m_btnLeft;
+    int m_btnXSpace;
+    int m_btnYSpace;
+
+    //Graphic
+    QLabel *m_pGraphicFrame;
+    QLabel *m_pGraphic;
+
+    //navigate item
+    TopNavigateWidget *m_pTopWidget;
+};
+
+#endif // LARGERAPHICONLYSHOW_H
