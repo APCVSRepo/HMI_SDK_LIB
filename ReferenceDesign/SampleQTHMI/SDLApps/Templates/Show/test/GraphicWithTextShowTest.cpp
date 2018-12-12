7cc79e94e25d8af80b77dda7f04881760945923d
zenghuan1 <huan.zeng1@pactera.com>
2018-12-12 14:09:01 +0800

    Correction of test parameters
    Signed-off-by: zenghuan1 <huan.zeng1@pactera.com>

--- ReferenceDesign/SampleQTHMI/SDLApps/Templates/Show/test/GraphicWithTextShowTest.cpp
+++ ReferenceDesign/SampleQTHMI/SDLApps/Templates/Show/test/GraphicWithTextShowTest.cpp
@@ -163,18 +163,15 @@ TEST_F(CGraphicWithTextShowTest,getShowData_Success)
     params["showStrings"] = stringsArr;
     params["appID"] = 3;
     params["alignment"] = "CENTERED";
+    ShowObj["params"] = params;
     appListMock.DelegateOnRequest(ShowObj);
     EXPECT_CALL(appListMock,onRequest(ShowObj)).Times(AtLeast(1));
     appListMock.onRequest(ShowObj);
 
     EXPECT_EQ(3,appListMock.getActiveApp()->getShowData()["params"]["appID"].asInt());
-
+    
     rpcValueInterface &pObj = appListMock.getActiveApp()->getShowData();
-
     EXPECT_FALSE(pObj.isNull());
-
-//    CGraphicWithTextShow cGraphicWithTextShow(&appListMock);
-//    cGraphicWithTextShow.show();
 
 }
 
