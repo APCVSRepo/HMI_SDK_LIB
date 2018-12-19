5d36381f55348d9858daa3b6b67603c43af9314a
zenghuan1 <huan.zeng1@pactera.com>
2018-12-19 14:19:39 +0800

    Optimization script
    Signed-off-by: zenghuan1 <huan.zeng1@pactera.com>

--- ReferenceDesign/SampleQTHMI/SDLApps/Templates/test/HMITest.sh
+++ ReferenceDesign/SampleQTHMI/SDLApps/Templates/test/HMITest.sh
@@ -1,6 +1,23 @@
+#!/bin/bash
+
 export LD_LIBRARY_PATH=.
 ./HMI_TEST
-lcov -c -o test.info -d ../../
-lcov --extract test.info '*/SDLApps/Templates/*' -o result.info
-genhtml result.info -o ../../TestResult
 
+LOG_F=cov_log.log
+
+if [ ! -f "${LOG_F}" ];then
+	touch ${LOG_F}
+else
+	rm -f ${LOG_F}
+	touch ${LOG_F}
+fi
+
+echo "Extraction coverage rate ..."
+lcov -c -o test.info -d ../../ > ${LOG_F}
+lcov --extract test.info '*/SDLApps/Templates/*' -o result.info > ${LOG_F}
+
+echo "Generate html ..."
+genhtml result.info -o ../../TestResult > ${LOG_F}
+echo "Summary coverage rate:"
+cat ${LOG_F} | grep -F "lines......"
+cat ${LOG_F} | grep -F "functions.."
